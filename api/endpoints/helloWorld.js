0b498bfec9620cb1e17b691e3b19ac0bf2b66be4
Huy Nguyen <huy9997@gmail.com>
2019-05-01 23:16:04 -0700

    common setup changes

--- api/endpoints/helloWorld.js
+++ api/endpoints/helloWorld.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const router  = express.Router();
-
-const helloWorld = require('./helloWorld')
-
-router.use('/',helloWorld);
+router.get('/', (req, res) => res.send('Hello World!'))
 
 
 module.exports = router;
--- client/src/screens/helloWorld.js
+++ client/src/screens/helloWorld.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import Routes from './routes';
+
 class App extends Component {
   render() {
     return (
-      <div>
-        <Routes/>
+      <div className="App">
+        <text>
+          Hello world
+        </text>
       </div>
     );
   }
