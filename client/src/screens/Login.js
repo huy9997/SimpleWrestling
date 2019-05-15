001b7648ec7cdd9e64c8c42234a7bafbd3447582
Huy Nguyen <huy9997@gmail.com>
2019-05-15 16:42:43 -0700

    added some functions for login

--- client/src/screens/Login.js
+++ client/src/screens/Login.js
@@ -1,12 +1,16 @@
-import React, { Component } from 'react';
-import Login from '../components/login'
-
+import React, { Component } from "react";
+import Login from "../components/login";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
   render() {
-    return (
-      <Login/> 
-    );
+    return <Login />;
   }
 }
 
--- client/src/screens/signin.js
+++ client/src/screens/signin.js
@@ -1,12 +1,19 @@
-import React, { Component } from 'react';
-import Signin from '../components/signin'
-
+import React, { Component } from "react";
+import Signin from "../components/signin";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      dateOfBirth: "",
+      weightClass: "",
+      username: "",
+      password: ""
+    };
+  }
   render() {
-    return (
-      <Signin/>
-    );
+    return <Signin />;
   }
 }
 
