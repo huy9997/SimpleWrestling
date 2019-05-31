2d852611bee9fbdcfb8030a476c10d0ca3b206df
Huy Nguyen <huy9997@gmail.com>
2019-05-31 13:03:13 -0700

    added a search function

--- db/search/searchEmail.js
+++ db/search/searchEmail.js
@@ -1,7 +1,7 @@
 const db = require("../connection");
 
 const searchEmail = value => {
-  const SEARCH_FOR_UNIQUE_EMAIL = `SELECT * FROM coaches WHERE email = $1`;
+  const SEARCH_FOR_UNIQUE_EMAIL = `SELECT * FROM users WHERE email = $1`;
   return db.query(SEARCH_FOR_UNIQUE_EMAIL, value);
 };
 
--- endpoints/signup.js
+++ endpoints/signup.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const signup = require("../db/signUp/");
+const search = require("../db/search");
+
+const bcrypt = require("bcrypt");
 
 router.post("/", (req, res) => {
   const {
@@ -26,18 +29,20 @@ router.post("/", (req, res) => {
     weight_class,
     type_of_account
   ];
-
-  // console.log(values);
-  signup
-    .User(values)
-    .then(query => {
-      console.log("success");
-      res.json(query);
-    })
-    .catch(err => {
-      console.log(err);
-      res.json(err);
-    });
+  console.log(values);
+  search.SearchEmail([values[2]]).then(searchResult => {
+    console.log(searchResult);
+  });
+  // signup
+  //   .User(values)
+  //   .then(query => {
+  //     console.log(query);
+  //     res.json("sucess");
+  //   })
+  //   .catch(err => {
+  //     console.log(err);
+  //     res.json(err);
+  //   });
 });
 
 module.exports = router;
