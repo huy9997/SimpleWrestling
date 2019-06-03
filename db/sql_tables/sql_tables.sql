1c9014e187a8c5effd3e59094da8381de96c5d11
Huy Nguyen <huy9997@gmail.com>
2019-06-03 14:18:01 -0700

    added the sql tables to the db folder

---
+++ db/sql_tables/sql_tables.sql
@@ -0,0 +1,69 @@
+CREATE TABLE "users"
+(
+  "id" SERIAL PRIMARY KEY,
+  "first_name" VARCHAR NOT NULL,
+  "last_name" VARCHAR,
+  "email" VARCHAR UNIQUE,
+  "password" VARCHAR,
+  "date_of_birth" DATE,
+  "team" VARCHAR,
+  "address" VARCHAR,
+  "weight_class" INTEGER,
+  "type_of_account" VARCHAR,
+  "active" BOOLEAN
+);
+
+CREATE TABLE "UserWrestler"
+(
+  "id" SERIAL PRIMARY KEY,
+  "weight_class" INTEGER,
+  "wins"INTEGER,
+  "losses" INTEGER,
+  "user_account" VARCHAR references Users(type_of_account)
+);
+
+CREATE TABLE "Tournament"
+(
+  "id" SERIAL PRIMARY KEY,
+  "name" VARCHAR,
+  "location" VARCHAR,
+  "description" VARCHAR,
+  "tournament_start_date" DATE,
+  "tournament_end_date" DATE,
+  "wrestling_style" VARCHAR,
+  "wrestling_level" VARCHAR,
+  "tournament_flier" VARCHAR,
+  "tournament_logo" VARCHAR,
+  "admin_account" VARCHAR REFERENCES Users(type_of_account),
+);
+
+CREATE TABLE "UserSignUpTournament"
+(
+  "id" SERIAL PRIMARY KEY,
+  "name" VARCHAR,
+  "weight_class" INTEGER,
+  "seeding_notes" VARCHAR,
+  "user_approved" BOOLEAN,
+  "user_id" INTEGER REFERENCES UsersWrestler(id),
+  "tournament_id" INTEGER REFERENCES Tournament(id)
+)
+CREATE TABLE "BracketMatches"
+(
+  "id" SERIAL PRIMARY KEY,
+  "bout_number" INTEGER,
+  "wrestler1_name" VARCHAR,
+  "wrestler1_previous_match_score" VARCHAR,
+  "wrestler1_record" VARCHAR,
+  "wrestler2_name" VARCHAR,
+  "wrestler2_previous_match_score" VARCHAR,
+  "wrestler2_record" VARCHAR,
+  "tournament_id" INTEGER REFERENCES tournament(id)
+);
+
+CREATE TABLE "BracketResults"
+(
+  "id" SERIAL PRIMARY KEY,
+  "wrestler1_score" INTEGER,
+  "wrestler2_score" INTEGER,
+  "bout_number" INTEGER REFERENCES BracketMatches(id)
+);
