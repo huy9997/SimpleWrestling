ad92a033c994d3c6453a0e016d3b3616d20ce724
Huy Nguyen <huy9997@gmail.com>
2019-06-03 22:49:14 -0700

    changes to sql tables

--- db/sql_tables/sql_tables.sql
+++ db/sql_tables/sql_tables.sql
@@ -34,29 +34,22 @@ CREATE TABLE Tournament
   "wrestling_level" VARCHAR,
   "tournament_flier" VARCHAR,
   "tournament_logo" VARCHAR,
+  "active" BOOLEAN,
   "admin_account" INTEGER REFERENCES Users(id)
 );
 
 CREATE TABLE UserSignUpTournament
 (
   "id" SERIAL PRIMARY KEY,
-  "name" VARCHAR,
-  "weight_class" INTEGER,
   "seeding_notes" VARCHAR,
-  "user_approved" BOOLEAN,
   "user_id" INTEGER REFERENCES UserWrestler(id),
   "tournament_id" INTEGER REFERENCES Tournament(id)
 );
 CREATE TABLE BracketMatches
 (
   "id" SERIAL PRIMARY KEY,
-  "bout_number" INTEGER,
-  "wrestler1_name" VARCHAR,
-  "wrestler1_previous_match_score" VARCHAR,
-  "wrestler1_record" VARCHAR,
-  "wrestler2_name" VARCHAR,
-  "wrestler2_previous_match_score" VARCHAR,
-  "wrestler2_record" VARCHAR,
+  "wrestler1_id" INTEGER,
+  "wrestler2_id" INTEGER,
   "tournament_id" INTEGER REFERENCES Tournament(id)
 );
 
