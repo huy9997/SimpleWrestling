b99958f07c630bc3ec03b7218aa11cc50d73df8d
Huy Nguyen <huy9997@gmail.com>
2019-06-05 09:15:46 -0700

    added createTournament variables and created tournament folder in db

--- endpoints/createTournaments.js
+++ endpoints/createTournaments.js
@@ -5,4 +5,33 @@ router.get("/", (req, res) => {
   res.json("secure page");
 });
 
+router.post("/", (req, res) => {
+  let active;
+  let admin_account = req.user[0].id;
+  const {
+    name,
+    location,
+    description,
+    tournament_start_date,
+    tournament_end_date,
+    wrestling_style,
+    wrestling_level,
+    tournament_flier,
+    tournament_logo
+  } = req.body;
+  const values = [
+    name,
+    location,
+    description,
+    tournament_start_date,
+    tournament_end_date,
+    wrestling_style,
+    wrestling_level,
+    tournament_flier,
+    tournament_logo,
+    active,
+    admin_account
+  ];
+});
+
 module.exports = router;
