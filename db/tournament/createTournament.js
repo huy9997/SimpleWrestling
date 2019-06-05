af06164094ec83cd889103b6e04dadb997666e34
Huy Nguyen <huy9997@gmail.com>
2019-06-05 09:31:05 -0700

    changes to the naming structure and the completed the create tournament query

--- db/tournament/createTournament.js
+++ db/tournament/createTournament.js
@@ -1,7 +1,21 @@
 const db = require("../connection");
 
-const searchUser = value => {
-  const SEARCH_FOR_USER = `SELECT * FROM users WHERE `;
+const createTournament = value => {
+  const SEARCH_FOR_USER = `INSERT INTO tournament (
+    name,
+    location,
+    description,
+    tournament_start_date,
+    tournament_end_date,
+    wrestling_style,
+    wrestling_level,
+    tournament_flier,
+    tournament_logo,
+    active,
+    admin_account,
+    ) 
+    VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11)`;
+  return db.query(SEARCH_FOR_USER, value);
 };
 
-module.exports = searchUser;
+module.exports = createTournament;
