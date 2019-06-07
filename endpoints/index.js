b9276b6d7f52796474fb954adae55b8e3f4b123d
Huy Nguyen <huy9997@gmail.com>
2019-06-07 09:00:07 -0700

    added the tournament bracket route

--- endpoints/index.js
+++ endpoints/index.js
@@ -5,6 +5,7 @@ const signup = require("./signup");
 const auth = require("./auth");
 const createTournament = require("./createTournament");
 const tournamentSignUp = require("./tournamentSignUp");
+const tournamentBrackets = require("./tournamentBrackets");
 
 let isAdmin = (req, res, next) => {
   if (req.isAuthenticated() && req.user[0].type_of_account == "admin") {
@@ -25,5 +26,6 @@ router.use("/signup", signup);
 router.use("/auth", auth);
 router.use("/createTournament", isAdmin, createTournament);
 router.use("/tournamentSignUp", isWrestler, tournamentSignUp);
+router.use("/tournamentBrackets", tournamentBrackets);
 
 module.exports = router;
