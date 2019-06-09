8ac8c12f7f45f29d53c21a6d8770d0fb8e9dc33e
Huy Nguyen <huy9997@gmail.com>
2019-06-08 20:36:23 -0700

    working on get route for passing info to frontend

--- endpoints/tournamentBrackets.js
+++ endpoints/tournamentBrackets.js
@@ -18,12 +18,39 @@ router.post("/", (req, res) => {
   tournaments.InsertBracketMatches();
 });
 
+router.get("/brackets", (req, res) => {
+  //send wrestler1_id, wrestler2_id and match info to frontend
+  const {
+    wrestler1_id,
+    wrestler2_id,
+    weight_class_id,
+    tournament_id
+  } = req.query;
+  let idValue = [wrestler1_id, wrestler2_id];
+  let storeWrestlerinfo = [];
+  for (let i = 0; i < idValue.length; i++) {
+    search
+      .SearchWrestler([idValue[i]])
+      .then(searchWrestlerResults => {
+        console.log(searchWrestlerResults);
+        storeWrestlerinfo.push(searchWrestlerResults);
+      })
+      .catch(err => {
+        res.json("there was been an error in finding the wrestlerid");
+      });
+  }
+  console.log(storeWrestlerinfo);
+  res.json("successful");
+});
+
 router.post("/test", (req, res) => {
   const { wrestleridArray, weight_class_id, tournament_id } = req.body;
   //wrestler id?
   let wrestler1Data, wrestler2Data;
   for (let i = 0; i < wrestleridArray.length; i++) {
-    search.SearchWrestler([wrestleridArray[i]]).then(searchResults => {});
+    search.SearchWrestler([wrestleridArray[i]]).then(searchResults => {
+      wrestler1Data;
+    });
   }
   let wrestler1_Data, wrestler2_Data;
   //search info for wrestler 1 and 2 insert into bracketmatches
