bd6c6ac752c81411a2adab2662d0a716a4a14838
Huy Nguyen <huy9997@gmail.com>
2019-06-11 23:27:09 -0700

    working on the login

---
+++ client/src/components/generalComponents/Input.js
@@ -0,0 +1,4 @@
+import React from "react";
+
+const input = () => {};
+export default input;
--- client/src/components/login/index.js
+++ client/src/components/login/index.js
@@ -1,6 +1,12 @@
 import React from "react";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
 const login = () => {
-  return <div />;
+  return (
+    <Grid container direction="row" justify="center" alignItems="center">
+      <Box borderRadius={12}>hello</Box>
+    </Grid>
+  );
 };
 
 export default login;
