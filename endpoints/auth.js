635e3c8d92b6e69979050e0a43273f496ce76bd9
Huy Nguyen <huy9997@gmail.com>
2019-06-16 12:05:36 -0700

    trying new ways to do auth to fix redirect issue

--- endpoints/auth.js
+++ endpoints/auth.js
@@ -2,13 +2,21 @@ let express = require("express");
 let router = express.Router();
 let passport = require("passport");
 
-router.post("/login", (req, res, next) => {
-  passport.authenticate("local", {
-    successRedirect: "/",
-    failureRedirect: "/login",
-    failureFlash: true
-  })(req, res, next);
+// router.post("/login", (req, res, next) => {
+//   passport.authenticate("local", {
+//     successRedirect: "/",
+//     failureRedirect: "/login",
+//     failureFlash: true
+//   })(req, res, next);
+// });
+
+router.post("/login", passport.authenticate("local"), function(req, res) {
+  // If this function gets called, authentication was successful.
+  // `req.user` contains the authenticated user.
+  console.log(req.user);
+  res.redirect("http://localhost:3000/");
 });
+
 router.get("/logout", (req, res) => {
   req.logout();
   req.flash("success_msg", "You are logged out");
