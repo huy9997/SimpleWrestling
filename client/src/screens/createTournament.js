167fd79274ab084a17cc1a56d2e321efc5516403
Huy Nguyen <huy9997@gmail.com>
2019-06-17 20:52:48 -0700

    created a create tournament page

---
+++ client/src/screens/createTournament.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import axios from "axios";
+import Button from "@material-ui/core/Button";
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <div />;
+  }
+}
+export default App;
