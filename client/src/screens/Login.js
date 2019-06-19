b4726c9a5fb6689614f42acedc1ee69ada7611d2
Eric Groom <egroom@mail.sfsu.edu>
2019-06-19 13:06:37 -0700

    organize imports

--- client/src/screens/Login.js
+++ client/src/screens/Login.js
@@ -1,17 +1,19 @@
 import React, { Component } from "react";
-import { Redirect } from "react-router-dom";
-import Avatar from "@material-ui/core/Avatar";
-import Button from "@material-ui/core/Button";
-import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
-import Link from "@material-ui/core/Link";
-import Grid from "@material-ui/core/Grid";
-import Box from "@material-ui/core/Box";
-import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
-import Typography from "@material-ui/core/Typography";
-import Container from "@material-ui/core/Container";
 import axios from "axios";
+import { Redirect } from "react-router-dom";
+import {
+  Avatar,
+  Button,
+  TextField,
+  FormControlLabel,
+  Checkbox,
+  Link,
+  Grid,
+  Box,
+  Typography,
+  Container
+} from "@material-ui/core";
+import { LockOutlinedIcon } from "@material-ui/icons";
 
 class App extends Component {
   constructor(props) {
