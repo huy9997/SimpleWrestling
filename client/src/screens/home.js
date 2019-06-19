81166b5d93e8c25636843c3db2f0c67d67da9add
Eric Groom <egroom@mail.sfsu.edu>
2019-06-19 13:00:58 -0700

    remove unused constructor

--- client/src/screens/home.js
+++ client/src/screens/home.js
@@ -2,11 +2,6 @@ import React, { Component } from "react";
 import axios from "axios";
 import Button from "@material-ui/core/Button";
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
   onsubmit = () => {
     console.log("submit a button");
     axios
