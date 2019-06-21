fbd1ae4c2a1407c3cddfaef44bf3aefe182cabb1
Huy Nguyen <huy9997@gmail.com>
2019-06-20 21:02:10 -0700

    adding tournament card

--- db/tournament/getTournamentCard.js
+++ db/tournament/getTournamentCard.js
@@ -1,7 +1,7 @@
 const db = require("../connection");
 
 const getTournamentCardData = value => {
-  const GET_TOURNAMENT_CARD_DATA = ``;
+  const GET_TOURNAMENT_CARD_DATA = `SELECT id, tournament_start_date, tournament_end_date, tournament_logo FROM  tournament where id=$1 `;
   return db.query(GET_TOURNAMENT_CARD_DATA, value);
 };
 
