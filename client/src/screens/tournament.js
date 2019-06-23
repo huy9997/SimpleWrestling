41447d1c02d48fc50c77b4ade57df7d2b465a794
Huy Nguyen <huy9997@gmail.com>
2019-06-23 11:17:43 -0700

    adding the create tournament button and the search bar not complete yet

--- client/src/screens/tournament.js
+++ client/src/screens/tournament.js
@@ -3,14 +3,19 @@ import axios from "axios";
 import Box from "@material-ui/core/Box";
 import Card from "../components/generalComponents/card";
 import Container from "@material-ui/core/Container";
+import { Button } from "@material-ui/core";
+import Input from "@material-ui/core/Input";
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      cards: []
+      cards: [],
+      search: ""
     };
   }
-
+  onchange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
   componentDidMount() {
     axios
       .get("/api/getTournamentCardData/tournament")
@@ -25,13 +30,20 @@ class App extends Component {
   }
 
   render() {
-    const { cards } = this.state;
+    const { cards, search } = this.state;
     return (
       <Container component="main" display="flex" flexDirection="column">
-        <Box>adding a carousel</Box>
-        <Box display="flex" flexDirection="row">
+        <Box display="flex" flexDirection="row-reverse">
+          <Button href="/createtournament"> Create Tournament</Button>
+        </Box>
+        <Input
+          placeholder="search"
+          onChange={e => this.onchange(e)}
+          value={search}
+        />
+        <Box display="flex" flexDirection="row" flexWrap="wrap">
           {cards.map(props => (
-            <Box m={1} width="100%">
+            <Box m={1}>
               <Card
                 title={props.name}
                 date={props.tournament_start_date}
