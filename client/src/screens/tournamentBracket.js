1d02b54d2a59973fc4660f2113f512411a93b1dd
Huy Nguyen <huy9997@gmail.com>
2019-06-23 19:18:12 -0700

    added the tournamentBracket page

---
+++ client/src/screens/tournamentBracket.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cards: []
+    };
+  }
+
+  render() {
+    return <div />;
+  }
+}
+export default App;
