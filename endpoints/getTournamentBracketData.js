db69807039283e48490c201232e6ec94e2b9a6b5
Huy Nguyen <huy9997@gmail.com>
2019-07-23 22:22:42 -0700

    playing with the backend to send wrestlerdata

--- endpoints/getTournamentBracketData.js
+++ endpoints/getTournamentBracketData.js
@@ -1,17 +1,25 @@
 let express = require("express");
 let router = express.Router();
 let tournaments = require("../db/tournament");
+let search = require("../db/search");
 
 router.get("/", (req, res) => {
   const { tournament_id, weight_class_id } = req.query;
   values = [tournament_id, weight_class_id];
+  let wrestlerBracketInfo = [];
   tournaments
     .GetTournamentBrackets(values)
     .then(tournamentBracketResults => {
-      res.json(tournamentBracketResults);
+      tournamentBracketResults.forEach(element => {
+        wrestlerBracketInfo.push(element.bout_number);
+      });
     })
     .catch(err => {
       res.json("error in getting  bracket data :(");
+    })
+    .then(results => {
+      console.log(wrestlerBracketInfo);
+      res.json(wrestlerBracketInfo);
     });
 });
 module.exports = router;
